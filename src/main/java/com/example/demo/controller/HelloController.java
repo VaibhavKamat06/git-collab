c447fa680d0e5db5c386ddf4a88026619a896ba1
vighnesh-gunaga <vighneshsg07@gmail.com>
2026-09-09 22:05:03 +0530

    changes in controller

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,5 +12,9 @@ public class HelloController {
     {
         return "Hello welcome to our first collab in github";
     }
-    
+    @GetMapping("/world")
+    public String world()
+    {
+        return "";
+    }
 }
